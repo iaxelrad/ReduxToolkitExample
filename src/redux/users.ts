67e97d83b8f4a4fef6062d988049f2cebea291b8
Itamar Axelrad <itamara@aman.co.il>
2022-05-11 19:47:09 +0300

    Setup Async Thunk

--- src/redux/users.ts
+++ src/redux/users.ts
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 
 interface UserData {
   id: number;
@@ -8,6 +8,11 @@ interface UserData {
   avatar: string;
 }
 
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await fetch('https://reqres.in/api/users?delay=1');
+  return (await response.json()).data as UserData[];
+});
+
 const usersSlice = createSlice({
   name: 'users',
   initialState: {
@@ -15,6 +20,18 @@ const usersSlice = createSlice({
     loading: false,
   },
   reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchUsers.pending, state => {
+      state.loading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.users = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(fetchUsers.rejected, state => {
+      state.loading = false;
+    });
+  },
 });
 
 export default usersSlice.reducer;
